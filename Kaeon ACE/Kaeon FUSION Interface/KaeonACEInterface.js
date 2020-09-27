fbc56dc0224ebc55acc48630747711395e81fe88
Jesse Dinkin <kaeon.ace@gmail.com>
2020-09-27 16:04:18 -0400

    Kaeon

--- Kaeon ACE/Kaeon FUSION Interface/KaeonACEInterface.js
+++ Kaeon ACE/Kaeon FUSION Interface/KaeonACEInterface.js
@@ -1,5 +1,14 @@
 var fusion = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/FUSION.js");
+var kaeonACE = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Kaeon-ACE/master/Kaeon%20ACE/API/Kaeon%20ACE/Babylon/KaeonACE.js");
 var philosophersStone = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Philosophers-Stone/master/Philosopher's%20Stone/API/JavaScript/PhilosophersStone.js");
+var widgets = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/UI/widgets.js");
+
+function getACECallback(fusion, ace) {
+
+	return function(element) {
+		kaeonACE.run(fusion, ace, element);
+	};
+}
 
 let ACEModule = function() {
 
@@ -11,10 +20,10 @@ let ACEModule = function() {
 
 	this.process = function(element, processed) {
 
-		require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Kaeon-ACE/master/Kaeon%20ACE/API/Kaeon%20ACE/Babylon/KaeonACE.js").run(
-			this.fusion,
-			element,
-			document.documentElement
+		widgets.createStartScreen(
+			document.documentElement,
+			"Start",
+			getACECallback(this.fusion, element)
 		);
 	}
 }
